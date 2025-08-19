0cb3e2c989fdbcb5fc366f48a876e6ae9e79d3ae
Priyansh Singh <157271597+Priyanshsingh1391@users.noreply.github.com>
2025-08-19 17:44:33 +0530

    Update Signup.jsx

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -12,7 +12,7 @@ const Signup = () => {
   const handleSubmit = async (e) => {
     e.preventDefault()
     try {
-      const response = await axios.post("https://note-app-api-six.vercel.app/api/auth/register", {
+      const response = await axios.post("http://localhost:3000/api/auth/register", {
         name,
         email,
         password
